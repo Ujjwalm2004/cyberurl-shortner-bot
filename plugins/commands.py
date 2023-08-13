3404ff3c493ece0580a9abff0c459584498c1983
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-08-13 14:30:12 +0530

    Create commands.py

---
+++ plugins/commands.py
@@ -0,0 +1,21 @@
+from pyrogram import Client, filters
+from database import *
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+
+@Client.on_message(filters.command('start'))
+async def start(_, m):
+  await m.reply_photo(
+    photo=PIC,
+    chat_id=int(m.from_user.id),
+    caption=START_TEXT.format(m.from_user.mention),
+    reply_markup=InlineKeyboardMarkup(
+      [[InlineKeyboardButton("Hᴇʟᴩ & Cᴏᴍᴍᴀɴᴅs Mᴇɴᴜ", callback_data="help")],
+       [InlineKeyboardButton("Cʜᴀɴɴᴇʟ", url="https://t.me/Cyber_Url"),
+        InlineKeyboardButton("Sᴜᴩᴩᴏʀᴛ", url="https://t.me/cyberstock_support")],
+       [InlineKeyboardButton("Eᴀʀɴ Mᴏɴᴇʏ", callback_data="earn_money")],
+       [InlineKeyboardButton("Dᴇᴠʟᴏᴩᴇʀ 👨‍💻", user_id=f"OWNER_ID"),
+        InlineKeyboardButton("Cʟᴏsᴇ ❌", callback_data="delete")]
+      ]))
+                            
+              
