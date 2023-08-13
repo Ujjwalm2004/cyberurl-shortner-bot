bbcaeafbfa6e324a534fdfac6df55d9bbb63b738
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-08-13 15:33:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,3 +23,18 @@ async def start(_, m):
     )
 
 
+@Client.on_message(filters.command('set_api') & filters.private)
+async def set_api(c, m):
+    n = await m.reply_text("Pʟᴇᴀsᴇ ᴡᴀɪᴛ...")
+    u_api = await db.get_api(m.from_user.id)
+    if u_api:
+        await n.edit(f"Yᴏᴜʀ ᴀᴩᴜ {u_api}")
+    if len(m.command) == 1:
+       return await m.reply_text("**Gɪᴠᴇ ᴍᴇ ʏᴏᴜʀ ᴀᴩɪ ᴡɪᴛʜ ᴄᴏᴍᴍᴀɴᴅ**\n\n**Exᴀᴍᴩʟᴇ - /set_api ᴀᴩɪ**\n\n**Gᴇᴛ ʏᴏᴜʀ ᴀᴩɪ ᴋᴇʏ [ʜᴇʀᴇ](https://cyberurl.me/member/tools/api)**")
+    user_id = m.from_user.id
+    api = m.text.split(' ', 1)[1]
+    await db.set_api(m.from_user.id, api=api)
+    await m.reply_text(f'**API ᴋᴇʏ sᴀᴠᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ!**\n\n**Yᴏʏʀ API: `{api}`**')
+
+
+
