018202a4fa3fb0146b43a1b3cc4c47ab6c16ab48
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-08-13 15:05:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,12 +10,13 @@ async def start(_, m):
         caption=START_TEXT.format(m.from_user.mention),
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("Hᴇʟᴩ & Cᴏᴍᴍᴀɴᴅs Mᴇɴᴜ", callback_data="help")],
+                [InlineKeyboardButton("Hᴇʟᴩ Mᴇɴᴜ", callback_data="help"),
+                 InlineKeyboardButton("Eᴀʀɴ Mᴏɴᴇʏ", callback_data="earn_money")],
                 [InlineKeyboardButton("Cʜᴀɴɴᴇʟ", url="https://t.me/Cyber_Url"),
                  InlineKeyboardButton("Sᴜᴩᴩᴏʀᴛ", url="https://t.me/cyberstock_support")],
-                [InlineKeyboardButton("Eᴀʀɴ Mᴏɴᴇʏ", callback_data="earn_money")],
-                [InlineKeyboardButton("Dᴇᴠʟᴏᴩᴇʀ 👨‍💻", user_id=f"{OWNER_ID}"),
-                 InlineKeyboardButton("Cʟᴏsᴇ ❌", callback_data="delete")]
+                [InlineKeyboardButton("Cʟᴏsᴇ ❌", callback_data="delete")]
             ]
         )
     )
+
+
