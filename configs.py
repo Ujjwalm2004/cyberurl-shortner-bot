14f0d73cc2c7ce02dfb54a7e6d5ebf71caa9fc05
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-08-13 14:12:59 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,5 +6,11 @@ API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 me = Client.get_me()
 username = me.username
+name = me.first_name
 BOT_USERNAME = os.environ.get("BOT_USERNAME", f"{username}")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+
+START_TXT = f'''Hᴇʟʟᴏ {}, I Aᴍ {name}!.
+๏ I ᴄᴀɴ Cᴏɴᴠᴇʀᴛ ʏᴏᴜʀ ʟɪɴᴋs ᴛᴏ Sʜᴏʀ ʟɪɴᴋs ᴜsɪɴɢ ʏᴏᴜ [ᴀᴩᴜ](https://cyberurl.m/member/tools/api).
+๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ Cᴏᴍᴍᴀɴᴅs Mᴇɴᴜ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs.
+'''
