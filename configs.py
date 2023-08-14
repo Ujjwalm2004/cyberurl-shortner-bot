437dbb8cce2e8881b6322fa270f9b4bf539bbbc7
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-08-14 18:52:29 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,13 @@
 import os
-from pyrogram import Client
 
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 PIC = os.environ.get("PIC", "")
-me = Client.get_me()
-username = me.username
-name = me.first_name
-BOT_USERNAME = os.environ.get("BOT_USERNAME", f"{username}")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "username")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 FSUB_CHANNEL = os.environ.get("FSUB_CHANNEL")
-START_TXT = f'''Hᴇʟʟᴏ {}, I Aᴍ {name}!.
+START_TXT = f'''Hᴇʟʟᴏ {}, I Aᴍ {}!.
 ๏ I ᴄᴀɴ Cᴏɴᴠᴇʀᴛ ʏᴏᴜʀ ʟɪɴᴋs ᴛᴏ Sʜᴏʀ ʟɪɴᴋs ᴜsɪɴɢ ʏᴏᴜ [ᴀᴩᴜ](https://cyberurl.m/member/tools/api).
 ๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ Hᴇʟᴩ Mᴇɴᴜ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs.
 '''
