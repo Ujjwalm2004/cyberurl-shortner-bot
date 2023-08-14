2a83f3e3eccda4e4e232c263da1414da719f0e71
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-08-14 18:53:00 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,7 +7,7 @@ PIC = os.environ.get("PIC", "")
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "username")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 FSUB_CHANNEL = os.environ.get("FSUB_CHANNEL")
-START_TXT = f'''Hᴇʟʟᴏ {}, I Aᴍ {}!.
+START_TXT = '''Hᴇʟʟᴏ {}, I Aᴍ {}!.
 ๏ I ᴄᴀɴ Cᴏɴᴠᴇʀᴛ ʏᴏᴜʀ ʟɪɴᴋs ᴛᴏ Sʜᴏʀ ʟɪɴᴋs ᴜsɪɴɢ ʏᴏᴜ [ᴀᴩᴜ](https://cyberurl.m/member/tools/api).
 ๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ Hᴇʟᴩ Mᴇɴᴜ ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴄᴏᴍᴍᴀɴᴅs.
 '''
