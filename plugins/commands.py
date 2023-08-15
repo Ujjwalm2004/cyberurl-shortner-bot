2451f16fe8e5b002b29c0c5c7e5c05ea04c6f965
Ujjwalm2004 <130154620+Ujjwalm2004@users.noreply.github.com>
2023-08-15 15:16:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,24 +6,6 @@ from configs import *
 import asyncio
 
 
-@Client.on_message(filters.command('set_api') & filters.private)
-async def set_api(c, m):
-    n = await m.reply_text("Pʟᴇᴀsᴇ ᴡᴀɪᴛ...")
-    u_api = await db.get_api(m.from_user.id)
-    if u_api:
-        await n.edit(f"Yᴏᴜʀ ᴀᴩᴜ {u_api}")
-    if len(m.command) == 1:
-       return await m.reply_text("**Gɪᴠᴇ ᴍᴇ ʏᴏᴜʀ ᴀᴩɪ ᴡɪᴛʜ ᴄᴏᴍᴍᴀɴᴅ**\n\n**Exᴀᴍᴩʟᴇ - /set_api ᴀᴩɪ**\n\n**Gᴇᴛ ʏᴏᴜʀ ᴀᴩɪ ᴋᴇʏ [ʜᴇʀᴇ](https://cyberurl.in/member/tools/api)**")
-    user_id = m.from_user.id
-    api = m.text.split(' ', 1)[1]
-    await db.set_api(m.from_user.id, api=api)
-    await m.reply_text(f'**API ᴋᴇʏ sᴀᴠᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ!**\n\n**Yᴏʏʀ API: `{api}`**')
-
-
-
-
-SPIC = "https://graph.org/file/ee3bcb029fa01979eda96.jpg"
-
 @Client.on_message(filters.command('start') & filters.private)
 async def start(_, m):
     await handle_private_message(client, message)
@@ -33,10 +15,11 @@ async def start(_, m):
 
     if cmd == "/start":
         try:
-            bot = await Client.get_me()
-            await m.reply_photo(
-                photo=SPIC,
-                caption=START_TEXT.format(m.from_user.mention, bot.mention),
+         #   bot = await Client.get_me()
+            await m.reply_text(
+                #photo=PIC,
+                START_TXT.format(m.from_user.mention), #bot.mention),
+                disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [InlineKeyboardButton("Hᴇʟᴩ Mᴇɴᴜ", callback_data="help"),
@@ -49,8 +32,16 @@ async def start(_, m):
             )
 
 
-#@Client.on_message(filters.command('set_api') & filters.private)
-#@Client.on_message(filtets.command('set_api') & filters.private)
-
-
+@Client.on_message(filters.command('set_api') & filters.private)
+async def set_api(c, m):
+    n = await m.reply_text("Pʟᴇᴀsᴇ ᴡᴀɪᴛ...")
+    u_api = await db.get_api(m.from_user.id)
+    if u_api:
+        await n.edit(f"Yᴏᴜʀ ᴀᴩᴜ {u_api}")
+    if len(m.command) == 1:
+       return await m.reply_text("**Gɪᴠᴇ ᴍᴇ ʏᴏᴜʀ ᴀᴩɪ ᴡɪᴛʜ ᴄᴏᴍᴍᴀɴᴅ**\n\n**Exᴀᴍᴩʟᴇ - /set_api ᴀᴩɪ**\n\n**Gᴇᴛ ʏᴏᴜʀ ᴀᴩɪ ᴋᴇʏ [ʜᴇʀᴇ](https://cyberurl.in/member/tools/api)**")
+    user_id = m.from_user.id
+    api = m.text.split(' ', 1)[1]
+    await db.set_api(m.from_user.id, api=api)
+    await m.reply_text(f'**API ᴋᴇʏ sᴀᴠᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ!**\n\n**Yᴏʏʀ API: `{api}`**')
 
